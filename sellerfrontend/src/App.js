30b14c7eea911d6d1f0c8a51b037185fc744f9e3
Vikas Kukreti <vickydvlpr@gmail.com>
2022-09-29 00:01:43 +0530

    App.js file formatted

--- sellerfrontend/src/App.js
+++ sellerfrontend/src/App.js
@@ -1,18 +1,43 @@
-import {BrowserRouter, Routes, Route,Navigate} from "react-router-dom"
-import Login from "./pages/login/Login"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter>
       <Routes>
-      <Route exact path="/" element={JSON.parse(localStorage.getItem('user')) ? <Home /> : <Register />}/>
-        <Route exact path="/login" element={JSON.parse(localStorage.getItem('user')) ? <Navigate to="/" /> : <Login />} />
-        <Route   exact path="/register" element={JSON.parse(localStorage.getItem('user')) ? <Navigate to="/" /> : <Register />} />
+        <Route
+          exact
+          path="/"
+          element={
+            JSON.parse(localStorage.getItem("user")) ? <Home /> : <Register />
+          }
+        />
+        <Route
+          exact
+          path="/login"
+          element={
+            JSON.parse(localStorage.getItem("user")) ? (
+              <Navigate to="/" />
+            ) : (
+              <Login />
+            )
+          }
+        />
+        <Route
+          exact
+          path="/register"
+          element={
+            JSON.parse(localStorage.getItem("user")) ? (
+              <Navigate to="/" />
+            ) : (
+              <Register />
+            )
+          }
+        />
       </Routes>
     </BrowserRouter>
-
   );
 }
 
